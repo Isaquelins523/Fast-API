31c63c209ef897bcd9b71862e24ed3a873b520bd
Isaque Lins <isaquelins523@gmail.com>
2025-04-13 22:29:55 -0300

    Criando schemas e edpoint de criação de usuarios

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,8 +1,17 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_api.schemas import Message,UserSchema, UserPublic
 
 app = FastAPI()
 
 
-@app.get('/', tags=['root'])
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message, tags=['root'])
 async def root():
     return {'message': 'Hello World'}
+
+@app.post('/user', status_code=HTTPStatus.CREATED, response_model=UserPublic, tags=['user'])
+def create_user(user: UserSchema):
+    return user
+
